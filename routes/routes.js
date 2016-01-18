c3fe06a5b76732f5f4c450d2ef1e158addda2829
theOrlin <theOrlin@gmail.com>
2016-01-18 15:45:53 +0200

    Add an Underscore dependency and separate routes in a file

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _ = require('underscore');
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+
+  });
+
+  app.post('/ingredients', function(req, res){
+
+  });
+};
--- server.js
+++ server.js
@@ -3,11 +3,16 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var routes = require('./routes/routes.js');
+var db = require('./db.js');
+routes(app);
 
-app.get('/', function(req, res) {
-    res.send('Food app root');
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('Express listening on port ' + PORT + '...');
+    });
+}, function(error) {
+    console.log('Database synchronization failed.');
 });
 
-app.listen(PORT, function(){
-    console.log('Express listening on port ' + PORT + '...');
-});
+
