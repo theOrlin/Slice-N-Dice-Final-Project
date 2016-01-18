a0c91a0200baf0397b92f8ce9edf685b923ed713
theOrlin <theOrlin@gmail.com>
2016-01-18 10:16:26 +0200

    Initialize repository

---
+++ app/server.js
@@ -0,0 +1,13 @@
+/* jshint node: true */
+'use strict';
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+    res.send('Food app root');
+});
+
+app.listen(PORT, function(){
+    console.log('Express listening on port ' + PORT + '...');
+});
