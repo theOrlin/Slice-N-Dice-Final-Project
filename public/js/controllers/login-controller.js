d3ebd35a9bf709ae71ce11567bf4edd0d9d709df
theOrlin <theOrlin@gmail.com>
2016-01-26 17:55:44 +0200

    Further fleshes out Login controller, retrieves and stores token to local storage now

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,17 +1,25 @@
 (function() {
     'use strict';
 
-    function LoginController(authService) {
+    function LoginController(authService, $location) {
         var vm = this;
         vm.user = {};
 
         vm.submit = function() {
-            authService.login(vm.user);
+            authService.login(vm.user)
+            .success(function(data, status, headers, config) {
+                localStorage.setItem('auth_token', headers('Auth'));
+                //console.log(headers('Auth'));
+                $location.path('/');
+            })
+            .error(function(error) {
+
+            });
         };
     }
 
 
-    LoginController.$inject = ['authService'];
+    LoginController.$inject = ['authService', '$location'];
 
     angular.module('foodApp.controllers')
         .controller('LoginController', LoginController);
