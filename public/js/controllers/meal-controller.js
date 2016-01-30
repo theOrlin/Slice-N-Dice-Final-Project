a63960d3368c40642a1178d2b7bf28dc7039b54f
theOrlin <theOrlin@gmail.com>
2016-01-30 18:01:38 +0200

    Redo meal parsing logic in Meal controller, add calculating totals functionality, delete meal button

--- public/js/controllers/meal-controller.js
+++ public/js/controllers/meal-controller.js
@@ -4,15 +4,22 @@
     var MealController = function(mealsService, $routeParams, ingredientsService) {
         var vm = this;
         vm.mealId = $routeParams.id;
-        vm.meal = {};
+        vm.calculatedMeal = {};
         vm.tempQuantity = 0;
         vm.selectedIngredient = null;
+        vm.sums = {
+            calories: 0,
+            fat: 0,
+            carbohydrates: 0,
+            protein: 0
+        };
 
         function init() {
             mealsService.getMeal(vm.mealId)
                 .success(function(meal) {
-                    vm.meal = meal;
-                    //console.log(meal);
+                    vm.originalMeal = meal;
+                    vm.calculatedMeal = mealsService.calculateValues(meal);
+                    calculateSums();
                 })
                 .error(function(error) {
 
@@ -23,7 +30,7 @@
 
         vm.getIngredientsByName = function(ingredientName) {
             return ingredientsService.getIngredientByName(ingredientName)
-                .then(function(response){
+                .then(function(response) {
                     return response.data;
                 });
         };
@@ -32,16 +39,48 @@
             vm.tempQuantity = $item.portionSize;
         };
 
-        vm.log = function(ingredient, quantity) {
+        vm.log = function(str) {
             console.log(ingredient.id);
         };
 
         vm.addIngredient = function(ingredient, quantity) {
             mealsService.addIngredient(vm.mealId, ingredient.id, quantity)
-            .success(function(response){
-                vm.selectedIngredient = null;
-                init();
-            });
+                .success(function(response) {
+                    vm.selectedIngredient = null;
+                    init();
+                });
+        };
+
+        vm.recalculateValues = function(newQuantity, index) {
+            newQuantity = parseInt(newQuantity);
+            var originalIngredient = vm.originalMeal.ingredients[index];
+            var ratio = newQuantity / originalIngredient.portionSize;
+
+            vm.calculatedMeal.ingredients[index].quantity = newQuantity;
+            vm.calculatedMeal.ingredients[index].calories = originalIngredient.calories * ratio;
+            vm.calculatedMeal.ingredients[index].fat = originalIngredient.fat * ratio;
+            vm.calculatedMeal.ingredients[index].carbohydrates = originalIngredient.carbohydrates * ratio;
+            vm.calculatedMeal.ingredients[index].protein = originalIngredient.protein * ratio;
+
+            calculateSums();
+        };
+
+        var calculateSums = function() {
+
+            vm.sums.calories = 0;
+            vm.sums.fat = 0;
+            vm.sums.carbohydrates = 0;
+            vm.sums.protein = 0;
+
+            var ingredients = vm.calculatedMeal.ingredients;
+
+            for (var i = 0; i < ingredients.length; i++) {
+                var ingredient = ingredients[i];
+                vm.sums.calories += ingredient.calories;
+                vm.sums.fat += ingredient.fat;
+                vm.sums.carbohydrates += ingredient.carbohydrates;
+                vm.sums.protein += ingredient.protein;
+            }
         };
     };
 
--- public/js/services/meals-service.js
+++ public/js/services/meals-service.js
@@ -17,6 +17,31 @@
         this.addIngredient = function(mealId, ingredientId, ingredientQuantity) {
             return $http.put('/api/meals/' + mealId, {id: ingredientId, quantity: ingredientQuantity});
         };
+
+        this.calculateValues = function(meal) {
+            var calculatedMeal = {};
+
+            calculatedMeal.id = meal.id;
+            calculatedMeal.name = meal.name;
+            calculatedMeal.ingredients = [];
+
+            for (var i = 0; i < meal.ingredients.length; i++) {
+                calculatedMeal.ingredients[i] = {};
+                var originalMeal = meal.ingredients[i];
+                var ratio = originalMeal.ingredientMeals.quantity / originalMeal.portionSize;
+                calculatedMeal.ingredients[i].id = originalMeal.id;
+                calculatedMeal.ingredients[i].name = originalMeal.name;
+                calculatedMeal.ingredients[i].portionSize = originalMeal.portionSize;
+                calculatedMeal.ingredients[i].measurement = originalMeal.measurement;
+                calculatedMeal.ingredients[i].quantity = originalMeal.ingredientMeals.quantity;
+                calculatedMeal.ingredients[i].calories = originalMeal.calories * ratio;
+                calculatedMeal.ingredients[i].fat = originalMeal.fat * ratio;
+                calculatedMeal.ingredients[i].carbohydrates = originalMeal.carbohydrates * ratio;
+                calculatedMeal.ingredients[i].protein = originalMeal.protein * ratio;
+            }
+
+            return calculatedMeal;
+        };
     };
 
     mealsService.$inject = ['$http'];
